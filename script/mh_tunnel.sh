91591bbc140185e2e948175569e93d4672431210
minerhome <minerhome.org@gmail.com>
2022-02-20 03:32:36 +0800

    Update mh_tunnel.sh

--- script/mh_tunnel.sh
+++ script/mh_tunnel.sh
@@ -11,9 +11,9 @@ mkdir /mh_tunnel
 cd /mh_tunnel
 
 
-wget --no-check-certificate https://github.com/minerhome/mh_tunnel/main/release/v2.2/mh_tunnel  
-wget --no-check-certificate https://github.com/minerhome/mh_tunnel/main/release/v2.2/config.yml
-wget --no-check-certificate https://github.com/minerhome/mh_tunnel/main/release/v2.2/encrypt.yml
+wget --no-check-certificate wget https://github.com/minerhome/mh_tunnel/raw/main/release/v2.2/mh_tunnel  
+wget --no-check-certificate wget https://github.com/minerhome/mh_tunnel/raw/main/release/v2.2/config.yml
+wget --no-check-certificate wget https://github.com/minerhome/mh_tunnel/raw/main/release/v2.2/encrypt.yml
 
 
 
