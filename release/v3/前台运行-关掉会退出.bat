d78ec780b69f87e114299099fc221f319bf5cb41
minerhome <minerhome.org@gmail.com>
2022-03-01 13:33:51 +0800

    基本上稳定了

---
+++ release/v3/前台运行-关掉会退出.bat
@@ -0,0 +1,12 @@
+
+
+call start  "mh_tunnel_minerhome.org" "mh_tunnel.exe"
+
+
+
+
+pause
+
+
+
+rem powershell Start-Process -WindowStyle hidden -FilePath "mh_tunnel.exe" 
---
+++ release/v3/后台运行-不懂不要点.bat
@@ -0,0 +1,10 @@
+
+
+powershell Start-Process -WindowStyle hidden -FilePath "mh_tunnel.exe" 
+
+rem powershell  Start-Job -ScriptBlock {mh_tunnel.exe}
+
+::pause
+
+
+
