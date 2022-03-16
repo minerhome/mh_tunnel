80df0b4290b99e2f3cc73641872f1fc0b9566d25
minerhome <minerhome.org@gmail.com>
2022-03-16 21:35:13 +0800

    Update inst_tunnel.sh

--- script/inst_tunnel.sh
+++ script/inst_tunnel.sh
@@ -7,18 +7,18 @@ cd /root/mh_tunnel
 
 
 
-wget https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/config.yml   
-wget https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/encrypt.yml  
-wget https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_tunnel  
-wget https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/tunnel  
-wget https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_tunnel.service   
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/config.yml   
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/encrypt.yml  
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_tunnel  
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/tunnel  
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_tunnel.service   
 
 
 
 
 
 chmod +x /root/mh_tunnel/*
-mv /root/mh_tunnel/mh_tunnel.service  /lib/systemd/system/mh_tunnel.service
+mv /root/mh_tunnel/mh_tunnel.service  /lib/systemd/system/
 
 systemctl restart mh_tunnel
 
