7e09217f6397742bdd8a156bd38eb7ce2f1803e1
minerhome <minerhome.org@gmail.com>
2022-03-18 15:10:20 +0800

    update

--- releases/v4.0.0/linux/inst_tunnel.sh
+++ releases/v4.0.0/linux/inst_tunnel.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 
-rm -rf /mh_tunnel/mh_tunnel
-mkdir /mh_tunnel
+rm -rf /root/mh_tunnel
+mkdir /root/mh_tunnel
 
-cd /mh_tunnel
+cd /root/mh_tunnel
 
-wget --no-check-certificate  https://cdn.jsdelivr.net/gh/minerhome/mh_tunnel@main/release/v4.0.0/mh_tunnel  -O /mh_tunnel/mh_tunnel
-wget --no-check-certificate  https://cdn.jsdelivr.net/gh/minerhome/mh_tunnel@main/release/v4.0.0/config.yml  -O /mh_tunnel/config.yml
-wget --no-check-certificate  https://cdn.jsdelivr.net/gh/minerhome/mh_tunnel@main/release/v4.0.0/encrypt.yml  -O /mh_tunnel/encrypt.yml
-wget --no-check-certificate  https://cdn.jsdelivr.net/gh/minerhome/mh_tunnel@main/release/v4.0.0/run_tunnel.sh  -O /mh_tunnel/run_tunnel.sh
-wget --no-check-certificate  https://cdn.jsdelivr.net/gh/minerhome/mh_tunnel@main/release/v4.0.0/mh_tunnel.service  -O /mh_tunnel/mh_tunnel.service
 
-chmod +x /mh_tunnel/*
-mv /mh_tunnel/mh_tunnel.service  /lib/systemd/system/mh_tunnel.service
 
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/config.yml   
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/encrypt.yml  
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_tunnel  
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_core  
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/mh_tunnel.service   
+wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/v4.0.0/linux/run_mh_tunnel.sh   
+
+
+
+
+
+chmod +x /root/mh_tunnel/*
+mv /root/mh_tunnel/mh_tunnel.service  /lib/systemd/system/
+
+systemctl enable mh_tunnel
 systemctl restart mh_tunnel
 
 
