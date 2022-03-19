e0b675bd157def6fc8484ada2661dd80e5940707
minerhome <minerhome.org@gmail.com>
2022-03-20 05:20:08 +0800

    update

--- scripts/server/inst_server.sh
+++ scripts/server/inst_server.sh
@@ -1,27 +1,95 @@
 #!/bin/bash
 
+[[ $(id -u) != 0 ]] && echo -e "请使用root权限运行安装脚本， 通过sudo su root切换再来运行" && exit 1
 
-rm -rf /root/mh_server
-mkdir /root/mh_server
-
-cd /root/mh_server
-
-
-
-wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/config.yml   
-wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/encrypt.yml  
-wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/mh_server
-
-
-wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/scripts/server/mh_server.service
-wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/scripts/server/run_mh_server.sh
+cmd="apt-get"
+if [[ $(command -v apt-get) || $(command -v yum) ]] && [[ $(command -v systemctl) ]]; then
+    if [[ $(command -v yum) ]]; then
+        cmd="yum"
+    fi
+else
+    echo "此脚本不支持该系统" && exit 1
+fi
 
 
-chmod +x /root/mh_server/*
-mv /root/mh_server/mh_server.service  /lib/systemd/system/
+check_done() {
+    if netstat -antpl | grep -q "mh_server"; then
+        echo -e "\n\n" 
+        echo -e "-----------------------------------"
+        echo -e "\n" 
+        echo -e "安装成功，端对端加密隧道解密端（服务端）已经在运行......" 
+        echo -e "\n" 
+        echo -e "\n" 
+        echo -e "-----------------------------------"
+        echo -e "\n" 
+    else        
+        echo -e "\n\n" 
+        echo "安装不成功，请重启后重新安装"   
+        echo "出现各种选择，请按 确认/OK"
+        echo -e "\n\n" 
+    fi      
+}
 
-systemctl enable mh_server
-systemctl restart mh_server
 
+change_limit(){
+    num="n"
+    if [ $(grep -c "root soft nofile" /etc/security/limits.conf) -eq '0' ]; then
+        echo "root soft nofile 102400" >>/etc/security/limits.conf
+        num="y"
+    fi
+
+    if [[ "$num" = "y" ]]; then
+        echo "连接数限制已修改为102400,重启服务器后生效"
+    else
+        echo -n "当前连接数限制："
+        ulimit -n
+    fi
+}
+
+
+install() {
+
+    ufw disable
+    $cmd update -y
+    $cmd install wget -y
+    $cmd install net-tools -y
+
+    rm -rf /root/mh_server
+    mkdir /root/mh_server
+    cd /root/mh_server
+
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/config.yml   
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/encrypt.yml  
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/proxy_pools.yml  
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/releases/mh_server/v1.0.0/mh_server
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/scripts/server/mh_server.service
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/master/scripts/server/run_mh_server.sh
+
+    chmod +x /root/mh_server/*
+    mv /root/mh_server/mh_server.service  /lib/systemd/system/
+
+    systemctl enable mh_server
+    systemctl restart mh_server
+
+
+}
+
+
+
+
+
+
+echo "======================================================="
+echo "端对端加密隧道解密端（服务端）一键安装工具 - 矿工之家 - minerhome.org"
+echo "默认安装到/root/mh_server"
+echo "如果安装不成功，则重启服务器后重新安装"
+echo "出现各种选择，请按 确认/OK"
+echo "======================================================="
+
+change_limit
+install
+check_done
+
+ 
 
 
