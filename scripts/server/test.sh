540e7275d220395cad9f38f65933a891b85ec7d4
minerhome <minerhome.org@gmail.com>
2022-04-10 06:57:58 +0800

    更新

--- scripts/server/test.sh
+++ scripts/server/test.sh
@@ -49,6 +49,68 @@ change_limit(){
 }
 
 
+uninstall() {
+        systemctl stop mh_server
+        systemctl disable mh_server
+        rm -rf /root/mh_server
+}
+
+
+
+install() {
+
+    ufw disable
+    $cmd update -y
+    $cmd install wget -y
+    $cmd install net-tools -y
+
+    rm -rf /root/mh_server
+    mkdir /root/mh_server
+    cd /root/mh_server
+
+    echo "请选择安装的版本"
+    echo "  1、v1.0.0"
+    echo "  2、v4.1.0"
+    echo "  3、v4.2.0"
+    read -p "$(echo -e "请输入[1-3]：")" choose
+    case $choose in
+    1)
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v1.0.0/config.yml  -O  /root/mh_server/config.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v1.0.0/encrypt.yml  -O  /root/mh_server/encrypt.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v1.0.0/proxy_pools.yml  -O  /root/mh_server/proxy_pools.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v1.0.0/mh_server  -O  /root/mh_server/mh_server
+        ;;
+    2)
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.1.0/config.yml  -O  /root/mh_server/config.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.1.0/encrypt.yml  -O  /root/mh_server/encrypt.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.1.0/proxy_pools.yml  -O  /root/mh_server/proxy_pools.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.1.0/mh_server  -O  /root/mh_server/mh_server
+        ;;
+    3)
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.2.0/config.yml  -O  /root/mh_server/config.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.2.0/encrypt.yml  -O  /root/mh_server/encrypt.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.2.0/proxy_pools.yml  -O  /root/mh_server/proxy_pools.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.2.0/mh_server  -O  /root/mh_server/mh_server
+        ;;
+    *)
+        echo "请输入正确的数字"
+        ;;
+    esac
+                
+    wget  --no-check-certificate  https://raw.githubusercontent.com/minerhome/mh_tunnel/main/scripts/server/mh_server.service  -O  /lib/systemd/system/mh_server.service        
+    chmod +x /root/mh_server/*
+    systemctl daemon-reload
+    systemctl enable mh_server
+    systemctl restart mh_server  &    
+    echo "正在检查是否安装成功，请耐心等待1分钟稍......"
+    sleep 30s
+    check_done
+
+}
+
+
+
+
 install() {
 
     ufw disable
@@ -76,20 +138,53 @@ install() {
 
 clear
 echo "======================================================="
-echo "端对端加密隧道 - 解密端（服务端）- 一键安装工具 - 矿工之家 - minerhome.org"
-echo "默认安装到/root/mh_server"
-echo "如果安装不成功，则重启服务器后重新安装"
-echo "出现各种选择，请按 确认/OK"
+
 echo "======================================================="
 change_limit
 install
-# clear
-echo "正在检查是否安装成功，请稍等......"
-sleep 20s
-check_done
 
- 
- 
+  
 
-
-echo "请重启机器，配置才会生效"
+echo "======================================================="
+echo "端对端加密隧道 - 解密端（服务端）- 一键安装工具 - 矿工之家 - minerhome.org"
+echo "如果安装不成功，则重启服务器后重新安装"
+echo "出现各种选择，请按 确认/OK"
+echo "  1、安装(默认安装到/root/mh_server)"
+echo "  2、卸载"
+echo "  3、更新"
+echo "  4、启动"
+echo "  5、重启"
+echo "  6、停止"
+echo "  7、解除linux系统连接数限制(需要重启服务器生效)"
+echo "  8、查看当前系统连接数限制"
+echo "======================================================="
+read -p "$(echo -e "请选择[1-8]：")" choose
+case $choose in
+1)
+    install
+    ;;
+2)
+    uninstall
+    ;;
+3)
+    update
+    ;;
+4)
+    start
+    ;;
+5)
+    restart
+    ;;
+6)
+    stop
+    ;;
+7)
+    change_limit
+    ;;
+8)
+    check_limit
+    ;;
+*)
+    echo "输入错误请重新输入！"
+    ;;
+esac
