343b870de2114df86ab899159b00a9a87315006a
minerhome <minerhome.org@gmail.com>
2022-04-19 13:38:01 +0800

    U

--- releases/mh_tunnel/v4.1.0/mh_tunnel.bat
+++ releases/mh_tunnel/v4.1.0/mh_tunnel.bat
@@ -6,9 +6,10 @@ ipconfig /flushdns
 
 reg add HKEY_CURRENT_USER\Console /v QuickEdit /t REG_DWORD /d 00000000 /f
 
-@REM start  mh_tunnel.exe
 
-start %~dp0mh_tunnel.exe
+start  %~dp0mh_tunnel.exe
+
+
  
 
 
