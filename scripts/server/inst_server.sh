95ae97555b747edf834832a8391ee61e538f3600
minerhome <minerhome.org@gmail.com>
2022-05-05 19:42:21 +0800

    Update inst_server.sh

--- scripts/server/inst_server.sh
+++ scripts/server/inst_server.sh
@@ -114,6 +114,7 @@ install() {
     echo "  4、v4.3.0"
     echo "  5、v4.5.0 - 可开启抽水功能"
     echo "  6、v4.6.0 - 可开启抽水功能"
+    echo "  7、v5.0.0 - 测试版"
     read -p "$(echo -e "请输入[1-6]：")" choose
     case $choose in
     1)
@@ -152,6 +153,13 @@ install() {
         wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.6.0/proxy_pools.yml  -O  /root/mh_server/proxy_pools.yml
         wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v4.6.0/mh_server  -O  /root/mh_server/mh_server
         ;;
+    6)
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v5.0.0/config.yml  -O  /root/mh_server/config.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v5.0.0/encrypt.yml  -O  /root/mh_server/encrypt.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v5.0.0/proxy_pools.yml  -O  /root/mh_server/proxy_pools.yml
+        wget  --no-check-certificate https://raw.githubusercontent.com/minerhome/mh_tunnel/main/releases/mh_server/v5.0.0/mh_server  -O  /root/mh_server/mh_server
+        ;;
+
     *)
         echo "请输入正确的数字"
         ;;
