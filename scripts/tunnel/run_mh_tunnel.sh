73d3e08201b3a2004eaae2067b528143ac9153d7
minerhome <minerhome.org@gmail.com>
2022-05-10 03:00:23 +0800

    1

--- scripts/tunnel/run_mh_tunnel.sh
+++ scripts/tunnel/run_mh_tunnel.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 sleep 0.1s
-while true; do
-    cd /root/mh_tunnel
-    chmod +x /root/mh_tunnel/*
-    ./mh_tunnel
-    sleep 10s
-done
+cd /root/mh_tunnel
+chmod +x /root/mh_tunnel/*
+./mh_tunnel  &
+
+
 
 
 
