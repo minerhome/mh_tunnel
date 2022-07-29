249d204962bddeebeaaf16a3705734053da112e5
minerhome <minerhome.org@gmail.com>
2022-07-30 01:08:58 +0800

    1

---
+++ releases/mh_tunnel/v7.0.0/设置.bat
@@ -0,0 +1,20 @@
+
+@echo off
+
+ipconfig /flushdns
+
+
+reg add HKEY_CURRENT_USER\Console /v QuickEdit /t REG_DWORD /d 00000000 /f
+
+start  mh_tunnel.exe --setup
+
+
+ 
+
+
+
+:::pause
+
+
+
+rem powershell Start-Process -WindowStyle hidden -FilePath "mh_tunnel.exe" 
